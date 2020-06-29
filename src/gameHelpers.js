e7a6cb1b135a238e6af124ee74e85baa59b9112e
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-29 22:01:42 +0300

    creating I tetromino

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,6 +1,9 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
+// Below is the arrow function that creates the stage for us:
+// The stage is a nested array / multi-dimensional array that represents rows and columns: 
+// This multi-dimensional array represents the grid
 export const createStage = () => 
   Array.from(Array(STAGE_HEIGHT), () => 
     new Array(STAGE_WIDTH).fill([0, 'clear'])
---
+++ src/tetrominos.js
@@ -0,0 +1,12 @@
+export const TETROMINOS = {
+    0: {shape: [0], color:'0, 0, 0'},
+    I: {
+        shape: [
+                [0, I, 0, 0],
+                [0, I, 0, 0],
+                [0, I, 0, 0],
+                [0, I, 0, 0]
+               ],
+        color: '80, 227, 230',
+    },
+}
