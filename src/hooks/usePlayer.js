fb208774a9a715ce5eac7b6e18ad904ff8a32ee4
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-07-03 20:28:44 +0300

    usePlayer state hook created

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -3,5 +3,16 @@ import { useState } from 'react';
 import { randomTetromino } from '../tetrominos';
 
 export const usePlayer = () => {
-    const [player, setPlayer] = useState()
+    const [player, setPlayer] = useState({
+        pos: { x: 0, y: 0 },
+        tetromino: randomTetromino().shape,
+        collided: false,
+    });
+    // const [player, setPlayer] = useState()
+    // the line above is achieving the same thing as the three lines below together.
+    // const playerState = useState();
+    // const player = playerState[0]
+    // const setPlayer = playerState[1]
+    
+    return [player];
 }
