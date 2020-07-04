4a0c1b952dc695d65fba8dbd42e36d00b7d4ab50
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-07-04 14:46:17 +0300

    bug fixed, startbutton was returning no tetromino after a number of presses

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -27,6 +27,6 @@ export const useStage = (player, resetPlayer) => {
         };
 
         setStage(prev => updateStage(prev));
-    }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
+    }, [player]);
     return [stage, setStage];
 };
