b3dd811c125876c7f1ec30743d63e4255515d51a
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-07-04 14:39:32 +0300

    updated the useStage hook

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -15,28 +15,31 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
+
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
   console.log('re-render');
 
   const movePlayer = dir => {
-
+    updatePlayerPos({ x: dir, y: 0 });
   }
 
   const startGame = () => {
-
+    // Reset everything
+    setStage(createStage());
+    resetPlayer();
   }
 
   const drop = () => {
-
+    updatePlayerPos({ x: 0, y: 1, collided: false})
   }
 
   const dropPlayer = () => {
-
+    drop();
   }
 
   const move = ({ keyCode }) => {
@@ -65,7 +68,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton onClick={startGame}/>
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import { randomTetromino } from '../tetrominos';
+import { STAGE_WIDTH } from '../gameHelpers';
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
@@ -14,5 +15,20 @@ export const usePlayer = () => {
     // const player = playerState[0]
     // const setPlayer = playerState[1]
     
-    return [player];
+    const updatePlayerPos = ({ x, y, collided}) => {
+        setPlayer(prev => ({
+            ...prev,
+            pos: { x: (prev.pos.x += x), y: (prev.pos.y += y)},
+            collided,
+        }))
+    }
+
+    const resetPlayer = useCallback(() => {
+        setPlayer({
+            pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+            tetromino: randomTetromino().shape,
+            collided: false,
+        })
+    }, [])
+    return [player, updatePlayerPos, resetPlayer];
 }
