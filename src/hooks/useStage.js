e5a9a3410e7ddf626136cb8e9744284f06292eae
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-07-04 20:39:00 +0300

    checking for collision in useStage

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -22,11 +22,15 @@ export const useStage = (player, resetPlayer) => {
                     }
                 });
             });
+            // Then check if we collided 
+            if(player.collided) {
+                resetPlayer();
+            }
 
             return newStage;
         };
 
         setStage(prev => updateStage(prev));
-    }, [player]);
+    }, [player, resetPlayer]);
     return [stage, setStage];
 };
