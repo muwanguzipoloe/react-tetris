5c452aa934ed82fd46754575510839dd6a2baa1a
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-07-07 01:51:12 +0300

    applied the gamestatus customhook and added speed increases to the game

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -9,6 +9,7 @@ import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 // Components
 import Stage from './Stage';
@@ -20,7 +21,10 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate ] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+    rowsCleared
+  );
 
   console.log('re-render');
 
@@ -37,9 +41,18 @@ const Tetris = () => {
     setDropTime(1000);
     resetPlayer();
     setGameOver(false);
-  }
+    setScore(0);
+    setRows(0);
+    setLevel(0);
+  };
 
   const drop = () => {
+    // Increase level when player has cleared 10 rows
+    if (rows > (level + 1) * 10) {
+      setLevel(prev => prev + 1 );
+      // Also increase the speed
+      setDropTime(1000 / (level + 1) + 200);
+    }
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false })
     } else {
@@ -57,7 +70,7 @@ const Tetris = () => {
     if (!gameOver) {
       if (keyCode === 40) {
         console.log("interval on");
-        setDropTime(1000);
+        setDropTime(1000 / (level + 1) + 200);
       }
     }
   }
