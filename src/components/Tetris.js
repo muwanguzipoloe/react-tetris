41090e952258f089c1f6b3f9aa78c1a8c08026f1
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-07-07 00:54:36 +0300

    created callback for when to change interval and re-enable it based on whether the drop key is pressed or not

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -53,7 +53,16 @@ const Tetris = () => {
     }
   }
 
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000);
+      }
+    }
+  }
+
   const dropPlayer = () => {
+    setDropTime(null);
     drop();
   }
 
@@ -76,7 +85,13 @@ const Tetris = () => {
   }, dropTime )
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper 
+      role="button" 
+      tabIndex="0" 
+      onKeyDown={e => move(e)} 
+      onKeyUp={keyUp}
+      >
+        
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
